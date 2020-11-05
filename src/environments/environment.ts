24ee6913b821b77ac809f5fcd30d55bec58bf45b
Billy Chepkemoi <cnafayo@gmail.com>
2020-11-05 21:04:56 +0300

    updating environ ts with access token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  _baseUrl: "https://api.github.com",
+  _myAccountToken: "91203e70a0718e148d4b03ac69eb01c39c748fe4"
 };
 
 /*
